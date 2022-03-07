f367fe05377117197441223775c841fe8ac5ddac
Osinachi Nwagboso Samuel <osinachinwagboso@gmail.com>
2022-03-07 18:25:42 +0000

    Implemented model for about page

--- IrisMed/Models/ContactUsModel.cs
+++ IrisMed/Models/ContactUsModel.cs
@@ -1,6 +1,33 @@
-﻿namespace IrisMed.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace IrisMed.Models
 {
-    public class ContactUsModel
+    public class ContactUsModel: PageModel
     {
+        public InputModel Input { get; set; }
+        public class InputModel
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [DataType(DataType.MultilineText)]
+            [Required]
+            public string Content { get; set; }
+        }
+
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
     }
 }
